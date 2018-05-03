c82c58337da80feb4a6b6cbe358bf0b01065bc46
Marcelo Vera Salvatierra <marcelo.verasalvatierra@gmail.com>
2018-05-03 16:47:47 -0300

    added ingredients component to burger builder controls

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import classes from './Burger.css'
+import BurgerIngridient from './BurgerIngredient/BurgerIngredient'
 const burger = (props) => {
   return (
-    <div></div>
+    <div className={classes.Burger}>
+      <BurgerIngridient type='bread-top'></BurgerIngridient>
+      <BurgerIngridient type='cheese'></BurgerIngridient>
+      <BurgerIngridient type='meat'></BurgerIngridient>
+      <BurgerIngridient type='bread-bottom'></BurgerIngridient>
+    </div>
   )
 }
 
