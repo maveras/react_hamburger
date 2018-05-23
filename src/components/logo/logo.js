d64d2dcd5e5711c077ee4b9d9bd7d0ccc8035152
Marcelo Vera Salvatierra <marcelo.verasalvatierra@gmail.com>
2018-05-23 15:34:05 -0400

    added logo component, css and modified toolbar to add logo compo

---
+++ src/components/logo/logo.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import logoImg from '../../assets/images/burger-logo.png'
+import classes from './Logo.css'
+const logo = () => (
+  <div className={classes.Logo}>
+    <img src={logoImg} alt="burgerLogo"/>
+  </div>
+)
+
+export default logo
