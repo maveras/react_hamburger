6822a8faf530f2428cac02415e7fb152ca12c556
Marcelo Vera Salvatierra <marcelo.verasalvatierra@gmail.com>
2018-07-11 10:33:30 -0400

    added spinner css loader

--- src/containers/burgerBuilder/BurgerBuilder.js
+++ src/containers/burgerBuilder/BurgerBuilder.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
+import axios from '../../hoc/axios-orders'
 
 import Aux from '../../hoc/Aux';
 import Burger from '../../components/Burger/Burger';
 import BuildControls from '../../components/Burger/BuildControls/BuildControls';
 import Modal from '../../components/UI/Modal/Modal';
 import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import Spinner from '../../components/UI/Spinner/Spinner'
 
 const INGREDIENT_PRICES = {
     salad: 0.5,
@@ -27,7 +29,8 @@ class BurgerBuilder extends Component {
         },
         totalPrice: 4,
         purchasable: false,
-        purchasing: false
+        purchasing: false,
+        loading: false
     }
 
     updatePurchaseState (ingredients) {
@@ -81,36 +84,66 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        alert('You continue!');
+        //alert('You continue!')
+        this.setState({loading: true})
+        const order = {
+          ingredients: this.state.ingredients,
+          price: this.state.totalPrice,
+          customer: {
+            name: 'Marcelo Vera',
+            address: {
+              street: 'Calle prueba1',
+              zipCode: 'COdigo postal',
+              country: 'Chile'
+            },
+            email: 'test@test.com'
+          },
+          deliveryMethod: 'faster'
+        }
+
+        axios.post('/orders.json', order)
+          .then((res)=> {
+            console.log(res)
+            this.setState({ loading: false, purchasing: false })
+          })
+          .catch(res => {
+            console.log(res)
+            this.setState({ loading: false, purchasing: false })
+          })
     }
 
     render () {
-        const disabledInfo = {
-            ...this.state.ingredients
-        };
-        for ( let key in disabledInfo ) {
-            disabledInfo[key] = disabledInfo[key] <= 0
-        }
-        // {salad: true, meat: false, ...}
-        return (
-            <Aux>
-                <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary 
-                        ingredients={this.state.ingredients}
-                        price={this.state.totalPrice}
-                        purchaseCancelled={this.purchaseCancelHandler}
-                        purchaseContinued={this.purchaseContinueHandler} />
-                </Modal>
-                <Burger ingredients={this.state.ingredients} />
-                <BuildControls
-                    ingredientAdded={this.addIngredientHandler}
-                    ingredientRemoved={this.removeIngredientHandler}
-                    disabled={disabledInfo}
-                    purchasable={this.state.purchasable}
-                    ordered={this.purchaseHandler}
-                    price={this.state.totalPrice} />
-            </Aux>
-        );
+      const disabledInfo = {
+          ...this.state.ingredients
+      };
+      for ( let key in disabledInfo ) {
+          disabledInfo[key] = disabledInfo[key] <= 0
+      }
+      let orderSummary = <OrderSummary
+        ingredients={this.state.ingredients}
+        price={this.state.totalPrice}
+        purchaseCancelled={this.purchaseCancelHandler}
+        purchaseContinued={this.purchaseContinueHandler} />
+
+      if (this.state.loading) {
+        orderSummary = <Spinner></Spinner>
+      }
+      // {salad: true, meat: false, ...}
+      return (
+          <Aux>
+              <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
+                {orderSummary}
+              </Modal>
+              <Burger ingredients={this.state.ingredients} />
+              <BuildControls
+                  ingredientAdded={this.addIngredientHandler}
+                  ingredientRemoved={this.removeIngredientHandler}
+                  disabled={disabledInfo}
+                  purchasable={this.state.purchasable}
+                  ordered={this.purchaseHandler}
+                  price={this.state.totalPrice} />
+          </Aux>
+      );
     }
 }
 
