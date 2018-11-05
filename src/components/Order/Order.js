f45d06569f89ac1ba97c24a359aef377e2c9af83
Marcelo Vera Salvatierra <marcelo.verasalvatierra@gmail.com>
2018-11-05 16:00:12 -0300

    added style to resume of ingredientes

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -10,11 +10,17 @@
         }
       )
     }
-    let ingredientMod = ingredients.map((ingredient, index) => <span key={index}>{ingredient.name} ({ingredient.value})</span>)
+    let ingredientMod = ingredients.map((ingredient, index) => <span style={{
+      textTransform: 'capitalize',
+      margin: '0 10px',
+      display: 'inline-block',
+      padding: '5px',
+      border: '1px solid #ccc'
+    }} key={index}>{ingredient.name} ({ingredient.value})</span>)
     console.log(ingredients)
     return (
       <div className={classes.Order}>
-        <p>Ingredients: {ingredientMod}</p>
+        <p >Ingredients: {ingredientMod}</p>
         <p>Price: <strong>USD ${Number.parseFloat(props.price).toFixed(2)}</strong></p>
       </div>
     )
