90d1bbce9f30eb988415142904efd7baf2a24dba
Marcelo Vera Salvatierra <marcelo.verasalvatierra@gmail.com>
2018-11-07 09:26:59 -0300

    added dynamics input component

--- src/components/Ui/Input/Input.js
+++ src/components/Ui/Input/Input.js
@@ -3,16 +3,25 @@ import classes from './Input.css'
 
 const input = (props) => {
   let inputElement = null
-  switch (props.inputType) {
-    case ('input'):
-      inputElement = <input className={classes.InputElement} {...props}/>
+  switch (props.elementType) {
+    case "input":
+      console.log("entre aki");
+      inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} />;
       break;
-    case ('textArea'):
-      inputElement = <textarea className={classes.InputElement} {...props} />
+    case "textArea":
+      inputElement = <textarea className={classes.InputElement} {...props.elementConfig} value={props.value} />;
+      break;
+    case "select":
+      inputElement = <select className={classes.InputElement} value={props.value}>
+          {props.elementConfig.options.map(option => (
+          <option key={option.value} value={option.value}>{option.displayValue}</option>
+          ))}
+        </select>;
       break;
 
     default:
-      inputElement = <input className={classes.InputElement} {...props} />
+      console.log("entre aki");
+      inputElement = <input className={classes.InputElement} {...props.elementConfig} value={props.value} />;
       break;
   }
   return (
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -6,62 +6,118 @@ import Input from '../../../components/UI/Input/Input'
 import axios from 'axios'
 class ContactData extends Component {
   state = {
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      postalCode: ''
+    orderForm: {
+      name: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Your name"
+        },
+        value: ""
+      },
+      street: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Your street"
+        },
+        value: ""
+      },
+      zipCode: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "code zip"
+        },
+        value: ""
+      },
+      country: {
+        elementType: "input",
+        elementConfig: {
+          type: "text",
+          placeholder: "Country"
+        },
+        value: ""
+      },
+      email: {
+        elementType: "input",
+        elementConfig: {
+          type: "email",
+          placeholder: "Your email"
+        },
+        value: ""
+      },
+      deliveryMethod: {
+        elementType: "select",
+        elementConfig: {
+          options: [
+            {
+              value: "fastest",
+              displayValue: "Fastest"
+            },
+            {
+              value: "Cheapest",
+              displayValue: "Cheap"
+            }
+          ]
+        },
+        value: ""
+      }
     },
     loading: false,
     purchasing: false
-  }
+  };
   // componentDidMount () {
   //   console.log('me monteeee', this.props)
   // }
-  orderHandler = (e) => {
-    e.preventDefault()
-    this.setState( { loading: true } );
-      const order = {
-        ingredients: this.props.ingredients,
-        price: this.props.totalPrice,
-        customer: {
-          name: 'Max Schwarzmüller',
-          address: {
-            street: 'Teststreet 1',
-            zipCode: '41351',
-            country: 'Germany'
-          },
-          email: 'test@test.com'
-        },
-        deliveryMethod: 'fastest'
-      }
-      axios.post('https://react-hamburger-89ecb.firebaseio.com/orders.json', order)
-        .then( response => {
-            this.setState( { loading: false } );
-            this.props.history.push('/')
-        } )
-        .catch( error => {
-            this.setState( { loading: false } );
-        } );
-  }
-  render () {
+  orderHandler = e => {
+    e.preventDefault();
+    this.setState({ loading: true });
+    const order = {
+      ingredients: this.props.ingredients,
+      price: this.props.totalPrice,
+      customer: {
+        email: "test@test.com"
+      },
+      deliveryMethod: "fastest"
+    };
+    axios
+      .post("https://react-hamburger-89ecb.firebaseio.com/orders.json", order)
+      .then(response => {
+        this.setState({ loading: false });
+        this.props.history.push("/");
+      })
+      .catch(error => {
+        this.setState({ loading: false });
+      });
+  };
+  render() {
+    const formElementsArray = [];
+    for (let key in this.state.orderForm) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.orderForm[key]
+      });
+    }
     let form = (
       <div className={classes.ContactData}>
         <h4>Enter your contact data</h4>
         <form action="">
-          <Input type="input" label="Nombre" placeholder="Ingrese su nombre"/>
-          {/* <input className={classes.input} type="text" name="name" placeholder="your name" />
-          <input className={classes.input} type="email" />
-          <input className={classes.input} type="text" name="street" />
-          <input className={classes.input} type="email" name="email" /> */}
-          <Button btnType="Success" clicked={this.orderHandler}>ORDER</Button>
+          {formElementsArray.map(formElement => (
+            <Input
+              key={formElement.id}
+              elementType={formElement.config.elementType}
+              elementConfig={formElement.config.elementConfig}
+              value={formElement.config.value}
+            />
+          ))}
+          <Button btnType="Success" clicked={this.orderHandler}>
+            ORDER
+          </Button>
         </form>
       </div>
-    )
-    return (
-      this.state.loading ? <Spinner /> : form
     );
+    return this.state.loading ? <Spinner /> : form;
   }
-
 }
 export default ContactData
